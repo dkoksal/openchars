97825f233e9bb091be5b4dee4bb0adff6e5eaae9
dyngar-n <dyngar-n@dyngar-n>
2013-11-08 23:58:40 -0500

    licensing information, started on CSS layout

--- getlist.js
+++ getlist.js
@@ -1,4 +1,5 @@
 <script>
+//@license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-V3-or-later
 $(document).ready(function(){
   $get('data/user/fig.xml', function(d)){
     ($(d).find('').each(function(){
@@ -8,4 +9,5 @@ $(document).ready(function(){
     });
   });
 });
+//license-end
 </script>
