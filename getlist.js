3566db05dac200f5ead4caf04c4ff99d7762248b
dyngar-sb <dyngar-sb@dyngar-sb.(none)>
2013-11-10 00:21:20 -0500

    More layout stuff

--- getlist.js
+++ getlist.js
@@ -1,4 +1,3 @@
-<script>
 //@license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-V3-or-later
 $(document).ready(function(){
   $get('data/user/fig.xml', function(d)){
@@ -10,4 +9,3 @@ $(document).ready(function(){
   });
 });
 //license-end
-</script>
