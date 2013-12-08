7d8d7974d2320ce01251bf9986240d8d9b92c456
dyngar <w4nder1n9w12ard@gmail.com>
2013-12-08 00:46:57 -0500

    satisfied with the interface for now.

--- contribute.js
+++ contribute.js
@@ -1,20 +1,4 @@
 //@license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-V3-or-later
 $(document).ready(function(){
-	$("#loadimage").mousedown(function(){
-		$(this).css("background","silver");
-	});
-	$("#loadimage").mouseup(function(){
-		$(this).css("background","grey");
-		$.post("contrib.php",function(data){
-			
-		});
-	});
-	$("#filelist").mousedown(function(){
-		
-	});
-	$("#filelist").mouseup(function(){
-		$("#file").click();
-		    //s("#prev").attr("src",);
-	});
 });
 //@license-end
